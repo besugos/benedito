c1dd378651372029c93de3e74c1ffe25bbc81055
besugos <besugos@gmail.com>
2024-07-19 18:51:54 -0300

    Initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import 'stylesheets/application.css.scss';
+import 'stylesheets/application.css';
+import * as bootstrap from "bootstrap"
